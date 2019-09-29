6d4f1d2991cafb8a269f9bcd908b4d477e6b0f6b
DanielFOBraga <danielbraguita@gmail.com>
2019-09-30 00:12:59 +0100

    Inclui exercicio relativo a TextBoxNumber

---
+++ TextBoxNumberControl/TextBoxNumberControl/TextBoxNumber.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextBoxNumberControl
+{
+    public partial class TextBoxNumber: TextBox
+    {
+        public TextBoxNumber()
+        {
+            InitializeComponent();
+        }
+        [Category("Intervalo"), Description("Valor mínimo do intervalo de valores.")]
+        public int Minimo { get; set; }
+
+        [Category("Intervalo"), Description("Valor máximo do intervalo de valores.")]
+        public int Maximo { get; set; }
+        protected override void OnValidating(CancelEventArgs e)
+        {
+            int valor;
+            if ((int.TryParse(this.Text, out valor)) == false)
+            {
+                e.Cancel = true;
+                //não sei o que fazem as linhas seguintes
+                this.SelectAll();
+                this.Focus();
+                throw new Exception("Erro - deve ser definido um número inteiro.");
+            }
+            else
+            {
+                if ((valor < Minimo) || (valor > Maximo))
+                {
+                    e.Cancel = true;
+                    this.SelectAll();
+                    this.Focus();
+                    //gerar evento
+                    ProcValorForaIntervalo(EventArgs.Empty);
+                }
+            }
+        }
+        public event EventHandler ValorForaIntervalo;
+
+        protected virtual void ProcValorForaIntervalo(EventArgs e)
+        {
+            EventHandler handler = ValorForaIntervalo;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+    }
+}
